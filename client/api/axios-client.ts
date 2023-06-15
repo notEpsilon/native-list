dab898e6e624230af9cc596b0222ed3f5399cda2
notEpsilon <ibrahimnasr393@gmail.com>
2023-06-15 09:37:48 +0300

    chore: removed ip module

--- client/api/axios-client.ts
+++ client/api/axios-client.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { isProduction } from "../utils/env.utils";
-import ip from "ip";
 
 const axs = axios.create({
   baseURL:
     process.env.API_URL ||
-    (!isProduction() ? `http://${ip.address()}:5000/api/v1` : ""),
+    (!isProduction() ? "http://192.168.1.7:5000/api/v1" : ""),
 });
 
 export { axs };
